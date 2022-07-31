c776a6628399a8016270516fb466d610c1c84341
jean <jean.canazart@aluno.ufop.edu.br>
2022-07-31 17:03:58 -0300

    ATUAL

---
+++ TCC/TCC/app/Http/Controllers/CadastroTime.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CadastroTime extends Controller
+{
+    //
+
+   public function cadastrar ()
+   {
+    return view ('times.cadastrar');
+   }
+
+
+
+}
