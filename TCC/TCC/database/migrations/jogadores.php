8c02aab798df5ae140fdc6f28a25b644a6860a34
jean <jean.canazart@aluno.ufop.edu.br>
2022-07-31 15:25:22 -0300

    Update jogadores.php

---
+++ TCC/TCC/database/migrations/jogadores.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jogadores', function (Blueprint $table) {
+            $table->id();
+            $table->string('cpf')->unique();
+            $table->string('nome');
+            $table->String('telefone');
+            $table->boolen('Eexcluido');
+            $table->date('nacimento');
+        
+            $table->timestamp('failed_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('failed_jobs');
+    }
+};
