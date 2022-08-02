1470485f183d984e7f3a845207268d8cf9bfb0d6
jean <jean.canazart@aluno.ufop.edu.br>
2022-08-02 14:05:40 -0300

    Update cadastrar.blade.php

--- TCC/TCC/resources/views/times/cadastrar.blade.php
+++ TCC/TCC/resources/views/times/cadastrar.blade.php
@@ -10,25 +10,29 @@
 
 <form class="row g-3">
 
+<div class="col-12">
+    <label for="nome" class="form-label">nome</label>
+    <input type="text" class="form-control" id="nome" placeholder="nome">
+  </div>
   <div class="col-md-6">
-    <label for="inputEmail4" class="form-label">Email</label>
+    <label for="email" class="form-label">Email</label>
     <input type="email" class="form-control" id="inputEmail4">
   </div>
   <div class="col-md-6">
-    <label for="inputPassword4" class="form-label">Password</label>
-    <input type="password" class="form-control" id="inputPassword4">
+    <label for="seaha" class="form-label">Senha</label>
+    <input type="password" class="form-control" id="Senha">
   </div>
   <div class="col-12">
-    <label for="inputAddress" class="form-label">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+    <label for="endereco" class="form-label">Endereço</label>
+    <input type="text" class="form-control" id="endereco" placeholder="Rua:..">
   </div>
   <div class="col-12">
     <label for="inputAddress2" class="form-label">Address 2</label>
     <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
   </div>
   <div class="col-md-6">
-    <label for="inputCity" class="form-label">City</label>
-    <input type="text" class="form-control" id="inputCity">
+    <label for="cidade" class="form-label">Cidade</label>
+    <input type="text" class="form-control" id="cidade">
   </div>
   <div class="col-md-4">
     <label for="inputState" class="form-label">State</label>
