3ae117d6c34eb84e9bcbbfa3d9a9611d01271fbb
jean <jean.canazart@aluno.ufop.edu.br>
2022-08-11 15:55:00 -0300

    Update 2022_07_31_183205_create_times_table.php

--- TCC/TCC/database/migrations/2022_07_31_183205_create_times_table.php
+++ TCC/TCC/database/migrations/2022_07_31_183205_create_times_table.php
@@ -23,7 +23,6 @@ return new class extends Migration
             $table->foreign('id_local')-> references('id')->on('local');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
-            $table->timestamp('atualizado-em')->useCurrent();
         });
     }
 
