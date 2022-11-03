0c44290e53f2eea0b597bf6a8c7bf85a15add2b1
jean <jean.canazart@aluno.ufop.edu.br>
2022-11-03 19:44:59 -0300

    Update jogadors.blade.php

--- TCC/TCC/resources/views/times/jogadors.blade.php
+++ TCC/TCC/resources/views/times/jogadors.blade.php
@@ -4,16 +4,17 @@
 <title>Jogador</title>
 
 @if(!empty($usuario))
-  <form class="row g-7" name="formEdit" id="formEdit" method="post" action="{{url("usuario/$usuario->id")}}">
+  <form class="row g-7 conterner" name="formEdit" id="formEdit" method="post" action="{{url("usuario/$usuario->id")}}">
   @method('PUT')
 
 @else
-  <form class="row g-7" name="formCadastro" id="formCadastro" method="post" action="{{url('cadastrojogador')}}"> 
+  <form class="row g-7 conterner" name="formCadastro" id="formCadastro" method="post" action="{{url('cadastrojogador')}}"> 
 @endif
 
-<form class="row g-3"  method="post"  action="{{url('cadastrojogador')}}"  >
+<form class="row g-3 conterner"  method="post"  action="{{url('cadastrojogador')}}"  >
 
-<div class="col-12">
+<div class="conterner py-4">
+<div class="col-8">
   <?php //var_dump($usuario); ?>
     <label for="nome" class="form-label">Nome Completo:</label>
     <input type="text" class="form-control" id="inNome" name='inNome' value="{{$usuario->nome ?? ''}}"  placeholder="Nome/Sobrenome">
@@ -26,15 +27,19 @@
     <label for="telefone" class="form-label">Telefone:</label>
     <input type="text" class="form-control" id="inTelefone" name='inTelefone' value="{{$usuario->telefone ?? ''}}" placeholder="( ) - ---- ----">
   </div>
+  <div class="col-md-4">
   <label for="inData" class="form-label">Data de nacimento</label>
-            <input type="date" class="form-control" name="inData" id="inData" value="{{isset($dados['inData']) ? $dados['inData'] : null}}" placeholder="DD/MM/AAAA">
-
-  <div class="col-md-6">
+   <input type="date" class="form-control" name="inData" id="inData" value="{{isset($dados['inData']) ? $dados['inData'] : null}}" placeholder="DD/MM/AAAA">
+   </div>
+  <div class="col-md-4">
     <label for="email" class="form-label">Email:</label>
     <input type="email" class="form-control" id="inEmail" name='inEmail' value="{{$usuario->email ?? ''}}" placeholder="....@email.com">
   </div>
 
   <div class="col-12">
       <button type="submit" class="btn btn-primary">Salvar</button>
+</div>
+</form>
   </div> 
+</div>
 @endsection('parte')
