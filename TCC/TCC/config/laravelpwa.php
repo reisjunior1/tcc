593c5a39b86cb0b22084f2725a44288903cb106d
Junior Reis <33906887+reisjunior1@users.noreply.github.com>
2022-12-15 18:54:28 -0300

    Adicionar PWA

---
+++ TCC/TCC/config/laravelpwa.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    'name' => 'LaravelPWA',
+    'manifest' => [
+        'name' => env('APP_NAME', 'LMF PWA'),
+        'short_name' => 'LMF PWA',
+        'start_url' => '/',
+        'background_color' => '#6dcdb1',
+        'theme_color' => '#009578',
+        'display' => 'standalone',
+        'orientation'=> 'any',
+        'status_bar'=> 'black',
+        'icons' => [
+            '192x192' => [
+                'path' => '/images/logo192.png',
+                'purpose' => 'any'
+            ],
+            '512x512' => [
+                'path' => '/images/logo512.png',
+                'purpose' => 'any'
+            ],
+        ],
+        'splash' => [
+            '640x1136' => '/images/icons/splash-640x1136.png',
+            '750x1334' => '/images/icons/splash-750x1334.png',
+            '828x1792' => '/images/icons/splash-828x1792.png',
+            '1125x2436' => '/images/icons/splash-1125x2436.png',
+            '1242x2208' => '/images/icons/splash-1242x2208.png',
+            '1242x2688' => '/images/icons/splash-1242x2688.png',
+            '1536x2048' => '/images/icons/splash-1536x2048.png',
+            '1668x2224' => '/images/icons/splash-1668x2224.png',
+            '1668x2388' => '/images/icons/splash-1668x2388.png',
+            '2048x2732' => '/images/icons/splash-2048x2732.png',
+        ],
+        'custom' => []
+    ]
+];
