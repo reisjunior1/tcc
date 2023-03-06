fab7a7e54d1bde53abdee678f0ecaf55078ed5f9
Junior Reis <33906887+reisjunior1@users.noreply.github.com>
2023-03-06 19:36:47 -0300

    Alterar Futebol para inicio na barra

--- TCC/TCC/resources/views/times/tela/telas.blade.php
+++ TCC/TCC/resources/views/times/tela/telas.blade.php
@@ -25,7 +25,7 @@
 <header>
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="{{ route('PaginaInicial') }}">Futebol</a>
+      <a class="navbar-brand" href="{{ route('PaginaInicial') }}">Início</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
